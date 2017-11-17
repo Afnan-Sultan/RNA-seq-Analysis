d65e8a07fb28c29e193a3fa37d78587b42698ddc
Afnan <32648719+Afnan-Sultan@users.noreply.github.com>
2017-11-17 11:57:35 +0200

    Update pipeline2_trail.sh

--- pipeline2_trail.sh
+++ pipeline2_trail.sh
@@ -1,32 +1,78 @@
 #!/bin/bash
-#implementing STAR and * 
+#impeleminting STAR and scallop
 
 #downloading STAR and unziping it
 wget https://github.com/alexdobin/STAR/archive/2.5.3a.tar.gz 
 tar xvzf chrX_data.tar.gz
 
-#downloading scripture
-wget ftp://ftp.broadinstitute.org/pub/papers/lincRNA/scripture-beta2.jar #download scripture
+#Installing scallop dependancies
+wget https://dl.bintray.com/boostorg/release/1.65.1/source/boost_1_65_1.tar.gz #getting boost folder
+tar xvzf boost_1_65_1.tar.gz
+
+wget https://zlib.net/zlib-1.2.11.tar.gz #getting & installing zlib required for htslib
+tar xvzf zlib-1.2.11.tar.gz 
+cd zlib-1.2.11/
+./configure
+make
+sudo make install
+cd
+
+git clone https://github.com/samtools/htslib #cloning & installing htslib
+cd htslib/
+autoheader
+autoconf
+./configure --disable-bz2 --disable-lzma --disable-gcs --disable-s3 --enable-libcurl=no
+make 
+sudo make install
+cd
+
+sudo apt-get install subversion #install subversion requiered for ClP
+svn co https://projects.coin-or.org/svn/Clp/stable/1.16 coin-Clp #getting & installing clp
+cd coin-Clp
+./configure --disable-bzlib --disable-zlib
+make
+sudo make install 
+cd
+
+export LD_LIBRARY_PATH=/home/$username/coin-Clp/lib:LD_LIBRARY_PATH #set Clp library to be available for shared libraries
+
+#Installing Scallop
+git clone https://github.com/Kingsford-Group/scallop
+cd scallop/ 
+autoreconf --install       	
+autoconf configure.ac
+./configure --with-clp=/home/$username/coin-Clp --with-htslib=/home/$username/htslib --with-boost=/home/$username/boost_1_65_1
+make
+cd
+
+#----------------------------------------------#
+
+#procedures 
 
 #copy chromosom X GTF file, chromosom X fasta file, and couple of read files -for simplicity- into STAR workspace
-cp $HOME/chrX_data/genes/chrX.gtf  $HOME/STAR-2.5.3a 
-cp $HOME/chrX_data/genome/chrX.fa  $HOME/STAR-2.5.3a
-cp /home/afnan/RNA_seq/chrX_data/samples/ERR188044_chrX_1.fastq.gz  /home/afnan/STAR-2.5.3a/ 
-cp /home/afnan/RNA_seq/chrX_data/samples/ERR188044_chrX_2.fastq.gz  /home/afnan/STAR-2.5.3a/
+cp $HOME/chrX_data/genes/chrX.gtf  /home/$username/STAR-2.5.3a 
+cp $HOME/chrX_data/genome/chrX.fa  /home/$username/STAR-2.5.3a
+cp $HOME/chrX_data/samples/ERR188044_chrX_1.fastq.gz  /home/$username/STAR-2.5.3a/ 
+cp $HOME/chrX_data/samples/ERR188044_chrX_2.fastq.gz  /home/$username/STAR-2.5.3a/
 
-#adding STAR binary file to PATH environment
-cp $HOME/STAR-2.5.3a/bin/Linux_x86_64/STAR /usr/bin 
+#adding STAR and scallop binary files to PATH environment
+cp /home/$username/STAR-2.5.3a/bin/Linux_x86_64/STAR /usr/bin
+cp /home/$username/scallop/src/scallop /usr/bin 
 
 #creating important folders
-cd $HOME/STAR-2.5.3a
+cd /home/$username/STAR-2.5.3a
 mkdir basic #to store the output of the process inside
 mkdir genome #create a folder to store generated indixes files
 
 #generating genome indexing
 cd basic/ 
-STAR --runThreadN 4 --runMode genomeGenerate --genomeDir $HOME/STAR-2.5.3a/genome/ --genomeFastaFiles $HOME/STAR-2.5.3a/chrX.fa --sjdbGTFfile $HOME/STAR-2.5.3a/chrX.gtf --sjdbOverhang 100
+STAR --runThreadN 4 --runMode genomeGenerate --genomeDir /home/$username/STAR-2.5.3a/genome/ --genomeFastaFiles /home/$username/STAR-2.5.3a/chrX.fa --sjdbGTFfile /home/$username/STAR-2.5.3a/chrX.gtf --sjdbOverhang 100
 
 #Mapping the reads 
-STAR --runThreadN 4 --genomeDir $HOME//STAR-2.5.3a/genome --sjdbGTFfile $HOME/STAR-2.5.3a/chrX.gtf --readFilesIn $HOME/STAR-2.5.3a/ERR188044_chrX_1.fastq.gz $HOME/STAR-2.5.3a/ERR188044_chrX_2.fastq.gz --readFilesCommand zcat
+STAR --runThreadN 4 --genomeDir /home/$username/STAR-2.5.3a/genome --sjdbGTFfile /home/$username/STAR-2.5.3a/chrX.gtf --readFilesIn /home/$username/STAR-2.5.3a/ERR188044_chrX_1.fastq.gz /home/$username/STAR-2.5.3a/ERR188044_chrX_2.fastq.gz --readFilesCommand zcat
 
+#Sorting and converting the output sam file into bam file
+samtools sort -@ 8 -o Aligned.out.bam Aligned.out.sam
 
+#transcriptom assembly
+scallop -i Aligned.out.bam -o Aligned.out.gtf 
