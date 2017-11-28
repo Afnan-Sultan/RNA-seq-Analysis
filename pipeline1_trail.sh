d1ae331b9b64496c32d97fed52ec27886c971c97
Afnan <32648719+Afnan-Sultan@users.noreply.github.com>
2017-11-28 19:45:21 +0200

    Update pipeline1_trail.sh

--- pipeline1_trail.sh
+++ pipeline1_trail.sh
@@ -3,6 +3,106 @@
 
 sudo apt-get updat #just a step to make sure everything is updated
 
+## modified pipeline 1
+
+#downloads and installment:  
+wget ftp://ftp.ccb.jhu.edu/pub/RNAseq_protocol/chrX_data.tar.gz #download data
+cd /home/afnan/Downloads #where username is your OS user name
+tar xvzf chrX_data.tar.gz #unzip it
+cd #getting out of the directory
+
+sudo apt-get install git #installing GitHub
+ 
+git clone https://github.com/infphilo/hisat2 #installing Hisat2
+
+wget https://github.com/samtools/samtools/releases/download/1.6/samtools-1.6.tar.bz2 -O - | tar xj #downloadeing samtools
+cd /home/$username/Downloads #where username is your OS user name 
+tar jxvf samtools-1.6.tar.bz2
+cd samtools-1.6
+make
+cd
+
+git clone https://github.com/gpertea/stringtie #installing StringTie
+cd stringtie
+make release
+cd
+
+git clone https://github.com/gpertea/gclib #installing dependancy of gffcompare
+git clone https://github.com/gpertea/gffcompare #installing gffcompare
+cd gffcompare
+make release
+cd
+
+#create a directory to store all the requiered inputs/outputs inside. we called the file created "RNA-seq".
+mkdir $HOME/RNA-seq #where $HOME is the desired path to place the directory at.  
+
+#copying data from chromosome X to our work directory, and samtools, Hisat, Stringtie & gffcompare to $PATH
+cp $HOME/chrX_dtat $HOME/RNA-seq #where $HOME is the path to our directory
+cp $HOME/samtools-01.6/samtools /usr/bin
+cp hisat2/hisat2* hisat2/*.py /usr/bin
+cp stringtie/stringtie /usr/bin
+cp gffcompare/gffcompare /usr/bin
+
+#genome indexing without gtf annotation
+mkdir $HOME/RNA-seq/index
+hisat2_extract_splice_sites.py $HOME/chrX_data/genome/chrX.fa > /home/$username/RNA_seq/splicesites.tsv
+hisat2_extract_exons.py $HOME/chrX_data/genome/chrX.fa > /home/$username/RNA_seq/exons.tsv
+hisat2-build -p 8 --ss /home/$username/RNA_seq/splicesites.tsv --exon /home/afnan/RNA_seq/exons.tsv 
+$HOME/chrX_data/genome/chrX.fa /$HOME/RNA_seq/index/index 
+
+# loop over the reads to map them for each sample to the reference genome:
+
+arr=(/home/afnan/chrX_data/samples/*)
+for ((i=0; i<${#arr[@]}; i=i+2)); do
+    #echo ${arr[$i]}, ${arr[$i+1]}
+    s=${arr[$i]}
+    v=${s:30:-11}.sam
+    #echo $v
+    ./hisat2 -p 8 --dta -x chrX_data/index/index -1 ${arr[$i]} -2 ${arr[$i+1]} -S $v
+b
+done
+# Sort and convert the SAM files to BAM:
+
+arr=(/home/afnan/RNA_seq/*.sam)
+for ((i=0; i<${#arr[@]}; i++)); do
+    #echo ${arr[$i]}, ${arr[$i+1]}
+    s=${arr[$i]}
+    #echo $s
+    v=${s:20:-4}.bam
+    #echo $v
+    samtools sort -@ 8 -o $v ${arr[$i]}
+done
+	
+
+# Assemble transcripts for each sample:
+
+arr=(/home/afnan/RNA_seq/*.bam)
+for ((i=0; i<${#arr[@]}; i++)); do
+    #echo ${arr[$i]}, ${arr[$i+1]}
+    s=${arr[$i]}
+    #echo $s
+    v=${s:20:-4}.gtf
+    w=${s:20:-9}
+    #echo $v
+    #echo $w
+    stringtie -p 8 -G chrX_data/genes/chrX.gtf -o $v -l $w ${arr[$i]}
+done
+
+# Merge transcripts from all samples:
+
+stringtie --merge -p 8 -G chrX_data/genes/chrX.gtf -o stringtie_merged.gtf chrX_data/mergelist.txt
+
+# Examine how the transcripts compare with the reference annotation:
+
+gffcompare -r chrX_data/genes/chrX.gtf -G -o merged stringtie_merged.gtf
+
+
+
+
+
+#---------------------------------------------------------------------------------------------------------#
+#old pipeline 1
+
 : <<'END'
 - downloads and installment:  
   1-  download the data using the following command on the terminal (The zip file is around 2GB) and unzip it.
@@ -13,7 +113,7 @@ sudo apt-get updat #just a step to make sure everything is updated
   #if you used the command $ sudo apt-get install samtools ; it'll install samtools-0.1.19, which will cost you more steps when applying the analysis. 
   5-  clone StringTie from GitHub and make it.
   6- clone gffcompare with the dependancy gclib and make release it.
-END
+
 
 wget ftp://ftp.ccb.jhu.edu/pub/RNAseq_protocol/chrX_data.tar.gz #download data
 tar xvzf chrX_data.tar.gz #unzip it
@@ -157,6 +257,8 @@ stringtie --merge -p 8 -G chrX_data/genes/chrX.gtf -o stringtie_merged.gtf chrX_
 # Examine how the transcripts compare with the reference annotation:
 
 gffcompare -r chrX_data/genes/chrX.gtf -G -o merged stringtie_merged.gtf 
+
+END
 
      
   
