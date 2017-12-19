619645bc770c0876284d2efbde681dd7016a72d2
Afnan <32648719+Afnan-Sultan@users.noreply.github.com>
2017-12-19 19:58:35 +0200

    Update hisat-stringtie.sh

--- hisat-stringtie.sh
+++ hisat-stringtie.sh
@@ -13,7 +13,7 @@ sudo cp $work_dir/programs_WorkDir/gffcompare/gffcompare /usr/bin
 mkdir $work_dir/hisat-stringtie   
 
 #genome indexing without gtf annotation
-mkdir $work_dir/hg38/hisat_index
+mkdir $work_dir/hg38_data/hisat_index
 hisat2-build -p 8 $work_dir/hg38_data/GRCh38.primary_assembly.genome.fa $work_dir/hg38_data/hisat_index/hg38
 
 #create directories for paper 1 liberaries/samples 
@@ -22,7 +22,6 @@ mkdir $work_dir/hisat-stringtie/paper1/poly_A $work_dir/hisat-stringtie/paper1/r
 mkdir $work_dir/hisat-stringtie/paper1/poly_A/reads_A $work_dir/hisat-stringtie/paper1/poly_A/reads_B $work_dir/hisat-stringtie/paper1/ribo-depleted/reads_A $work_dir/hisat-stringtie/paper1/ribo-depleted/reads_B
 
 # loop over the paired reads from each sample to map them to the reference genome:
-
 for a in $work_dir/download_data_RNA-seq_TM-AS/RNA-seq_data/*; do
     x=$(echo "$(basename $a)")
     for b in $a/reads_*; do
@@ -33,12 +32,11 @@ for a in $work_dir/download_data_RNA-seq_TM-AS/RNA-seq_data/*; do
     v=$(echo "$(basename $s)"| sed s/_1.fastq.gz/.sam/) #naming the output fila based on the input file. basename is to get the file name without the path, and sed is to replace the extention. 
     
     hisat2 -p 8 --dta -x $work_dir/hg38/hisat_index/hg38 -1 ${arr[$i]} -2 ${arr[$i+1]} -S $work_dir/hisat-stringtie/paper1/$x/$y/$v
-done  
-done
+       done  
+    done
 done
 
 # Sort and convert the SAM files to BAM:
-
 for a in $work_dir/hisat-stringtie/paper1/*; do
     for b in $a/reads_*; do
     arr=($b/*.sam)
@@ -47,13 +45,12 @@ for a in $work_dir/hisat-stringtie/paper1/*; do
     v=$(echo "$(basename $s)"| sed s/.sam/.bam/)
 
     samtools sort -o $b/$v ${arr[$i]}
-done
-done      
+        done
+    done      
 done
 
 
 # Assemble transcripts for each sample:
-
 for a in $work_dir/hisat-stringtie/paper1/*; do
     for b in $a/reads_*; do
     arr=($b/*.bam)
@@ -63,24 +60,31 @@ for a in $work_dir/hisat-stringtie/paper1/*; do
     w=$(echo "$(basename $s)"| sed s/.bam//)
 
     stringtie -o $b/$v -l $w ${arr[$i]}
-done
-done      
+        done
+    done      
 done
 
 
-#create a directory to store the final transcript gff stat 
+#create a directory to store th merged transcripts, stats and intersection with the differnet genome regions.  
 mkdir $work_dir/hisat-stringtie/final_output
 mkdir $work_dir/hisat-stringtie/final_output/paper1
 
 # Merge transcripts from all samples:
-stringtie --merge $work_dir/hisat-stringtie/paper1/poly_A/reads_A/*.gtf -o $work_dir/hisat-stringtie/final_output/paper1/p1_sA_poly_stringtie_merged.gtf
-stringtie --merge $work_dir/hisat-stringtie/paper1/poly_A/reads_B/*.gtf -o $work_dir/hisat-stringtie/final_output/paper1/p1_sB_poly_stringtie_merged.gtf
-stringtie --merge $work_dir/hisat-stringtie/paper1/ribo_depleted/reads_A/*.gtf -o $work_dir/hisat-stringtie/final_output/paper1/p1_sA_ribo_stringtie_merged.gtf
-stringtie --merge $work_dir/hisat-stringtie/paper1/ribo_depleted/reads_B/*.gtf -o $work_dir/hisat-stringtie/final_output/paper1/p1_sB_ribo_stringtie_merged.gtf
+# Merge transcripts from all samples:
+for a in $work_dir/star-scallop/paper1/*; do
+    x=p1_
+    y=$(echo "$(basename $a"_")"
+    for b in $a/reads_*; do
+    z=$(echo "$(basename $b"_")"
+    
+stringtie --merge $b/*.gtf -o $work_dir/hisat-stringtie/final_output/paper1/$x$y$z"stringtie_merged.gtf"
+    done
+done
 
-# Examine how the transcripts compare with the reference annotation
+#create a directory to store the final transcript gff stat
 mkdir $work_dir/hisat-stringtie/final_output/paper1/gffcompare 
 
+# Examine how the transcripts compare with the reference annotation
 for dir in $work_dir/hisat-stringtie/final_output/paper1/*.gtf; do 
     v=$(echo "$(basename $dir)"| sed s/stringtie_merged.gtf//)
 gffcompare -r $work_dir/hg38_data/gencode.v27.annotation.gtf -o $work_dir/hisat-stringtie/final_output/paper1/gffcompare/$v $dir
