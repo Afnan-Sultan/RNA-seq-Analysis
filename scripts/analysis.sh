45ec283791196dcae9fa5d37dd85c30416bc2b7a
Afnan <32648719+Afnan-Sultan@users.noreply.github.com>
2017-12-22 22:35:47 +0200

    Update analysis.sh

--- scripts/analysis.sh
+++ scripts/analysis.sh
@@ -1,55 +1,32 @@
 #!/bin/bash
 
-
-# Examine how the transcripts compare with the reference annotation
 for pipeline in $work_dir/*
     pipeline_name=$(echo "$(basename $pipeline)")
     if [[ $pipeline_name == hisat* || $pipeline_name == star* ]]; then
     for paper_dir in $pipeline_dir/final_output/*; do
+        if [ -d $paper_dir ]; then
         paper_name=$(echo "$(basename $paper_dir)")
         for gtf_file in $paper_dir/*.gtf; do 
-            output=$(echo "$(basename $gtf_file)"| sed s/stringtie_merged.gtf//)
-            gffcompare -r $work_dir/hg38_data/gencode.v27.annotation.gtf -o $work_dir/$pipeline_name/final_output/$paper_name/$output $gtf_file
-        done
-        fi
-    done
-    fi
-done
-
-#convert merged gtf files to bed files
-for pipeline in $work_dir/*; do
-    pipeline_name=$(echo "$(basename $pipeline)")
-    if [[ -d $pipeline && $pipeline_name == hisat* || $pipeline_name == star* ]]; then
-    for paper_dir in $pipeline_dir/final_output/*; do   if [[ -d $paper_dir ]]; do
-        paper_name=$(echo "$(basename $paper_dir)")
-        if [[ -d $paper_dir ]]; then 
-        for gtf_file in $paper_dir/*.gtf; do 
-            output=$(echo "$(basename $gtf_file)"| sed s/.gtf/.bed/)
+        
+            # Examine how the transcripts compare with the reference annotation
+            gff_output_prefix=$(echo "$(basename $gtf_file)"| sed s/stringtie_merged.gtf//)
+            gffcompare -r $work_dir/hg38_data/gencode.v27.annotation.gtf -o $work_dir/$pipeline_name/final_output/$paper_name/$gff_output_prefix $gtf_file
+        
+            #convert merged gtf file to bed file
+            bed_output=$(echo "$(basename $gtf_file)"| sed s/.gtf/.bed/)
             cat $gtf_file| 
             awk 'BEGIN{OFS="\t";} {print $1,$4-1,$5}' | 
-            sortBed | > $work_dir/$pipeline_name/final_output/$paper_name/$output
+            sortBed | > $work_dir/$pipeline_name/final_output/$paper_name/$bed_output
+            
+            #applying bedtools analysis
+            bedtools_input=$work_dir/$pipeline_name/final_output/$paper_name/$bed_output
+            bedtools_output=$(echo "$(basename $gtf_file)"| sed s/.gtf/_intersect_/)
+            intersectBed -a $work_dir/bedtools/hg38_exons.bed -b $bedtools_input > $work_dir/$pipeline/final_output/$paper_name/$bedtools_output"exons.bed"
+            intersectBed -a $work_dir/bedtools/hg38_introns.bed -b $bedtools_input > $work_dir/$pipeline/final_output/$paper_name/$bedtools_output"introns.bed"
+            intersectBed -a $work_dir/bedtools/hg38_intergenic.bed -b $bedtools_input > $work_dir/$pipeline/final_output/$paper_name/$bedtools_output"intergenic.bed"
+
         done
         fi
     done
     fi
 done
-
-#applying bedtools analysis
-for pipeline in $work_dir/*; do
-    pipeline_name=$(echo "$(basename $pipeline)")
-    if [[ -d $pipeline && $pipeline_name == hisat* || $pipeline_name == star* ]]; then
-    for paper_dir in $pipeline_dir/final_output/*; do
-        paper_name=$(echo "$(basename $paper_dir)")
-        if [[ -d $paper_dir ]]; then 
-        for bed_file in $paper_dir/*.bed; do 
-            output=$(echo "$(basename $dir)"| sed s/.bed/_intersect_/)
-            intersectBed -a $work_dir/bedtools/hg38_exons.bed -b $bed_file > $work_dir/$pipeline/final_output/$paper_name/$output"exons.bed"
-            intersectBed -a $work_dir/bedtools/hg38_introns.bed -b $bed_file > $work_dir/$pipeline/final_output/$paper_name/$output"introns.bed"
-            intersectBed -a $work_dir/bedtools/hg38_intergenic.bed -b $bed_file > $work_dir/$pipeline/final_output/$paper_name/$output"intergenic.bed"
-        done
-        fi
-    done
-    fi
-done
-
-
