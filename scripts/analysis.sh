bf0dba78c17031994cd8b8f6deb58f2acf4b115d
Afnan <32648719+Afnan-Sultan@users.noreply.github.com>
2018-01-17 10:56:52 +0200

    Update analysis.sh

--- scripts/analysis.sh
+++ scripts/analysis.sh
@@ -5,24 +5,25 @@ index_dir_path="$2"
 bedtools_dir="$3"
 paper_name=$(echo "$(basename $paper_dir)")
 
-# Examine how the transcripts compare with the reference annotation        
-for gtf in $paper_dir/*.gtf; do 
-    gff_output_prefix=$(echo "$(basename $gtf_file)"| sed s/stringtie_merged.gtf//)
-    gffcompare -r $index_dir_path/gencode.v27.annotation.gtf -o $paper_dir/$gff_output_prefix $gtf 
+# Examine how the transcripts compare with the reference annotation 
+mkdir -p $paper_dir/gffCompare       
+for gtf in $paper_dir/GTFs/*merged.gtf; do 
+    gff_output_prefix=$(echo "$(basename $gtf)"| sed s/merged.gtf//)
+    gffcompare -r $index_dir_path/gencode.v27.annotation.gtf -o $paper_dir/gffCompare/$gff_output_prefix $gtf 
 done
 
 #convert merged gtf file to bed file
-for gtf in $paper_dir/*.gtf; do            
+for gtf in $paper_dir/*merged.gtf; do            
     bed_output=$(echo "$(basename $gtf)"| sed s/.gtf/.bed/)
-    cat $gtf_file| 
+    cat $gtf| 
     awk 'BEGIN{OFS="\t";} {print $1,$4-1,$5}' | 
-    sortBed | > $paper_dir/$bed_output
+    sortBed > $bedtools_dir/$bed_output
 done  
          
 #applying bedtools analysis
-for bed in $paper_dir/*.bed; do 
+for bed in $bedtools_dir/*merged.bed; do 
     bedtools_output=$(echo "$(basename $bed)"| sed s/.bed/_intersect_/)
-    intersectBed -a $bedtools_dir/hg38_exons.bed -b $bedtools_input > $paper_dir/$bedtools_output"exons.bed"
-    intersectBed -a $bedtools_dir/hg38_introns.bed -b $bedtools_input > $paper_dir/$bedtools_output"introns.bed"
-    intersectBed -a $bedtools_dir/hg38_intergenic.bed -b $bedtools_input > $paper_dir/$bedtools_output"intergenic.bed"
+    intersectBed -a $bedtools_dir/hg38_exons.bed -b $bed > $paper_dir/$bedtools_output"exons.bed"
+    intersectBed -a $bedtools_dir/hg38_introns.bed -b $bed > $paper_dir/$bedtools_output"introns.bed"
+    intersectBed -a $bedtools_dir/hg38_intergenic.bed -b $bed > $paper_dir/$bedtools_output"intergenic.bed"
 done
