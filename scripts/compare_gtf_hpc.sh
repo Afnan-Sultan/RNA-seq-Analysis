6e73eb36857fc78bfe913276d38f072d4ba521aa
Afnan <32648719+Afnan-Sultan@users.noreply.github.com>
2018-01-27 14:15:52 +0200

    Update compare_gtf_hpc.sh

--- scripts/compare_gtf_hpc.sh
+++ scripts/compare_gtf_hpc.sh
@@ -24,19 +24,34 @@ for gtf in $gtf_dir/*merged.gtf; do
 done
 
 module swap GNU GNU/4.4.5; module load BEDTools/2.24.0;
+echo "converting gtf to bed ....... "
 #convert merged gtf file to bed file
 for gtf in $gtf_dir/*merged.gtf; do            
     bed_output=$(echo "$(basename $gtf)"| sed s/.gtf/.bed/)
     cat $gtf| 
     awk 'BEGIN{OFS="\t";} {print $1,$4-1,$5}' | 
+    sort -k1,1 -k2,2n |
     sortBed |
     mergeBed -i - > $bedtools_dir/$bed_output
-done  
-         
+done 
+
+echo "performing intersection ....... "      
 #applying bedtools analysis
 for bed in $bedtools_dir/*merged.bed; do 
     bedtools_output=$(echo "$(basename $bed)"| sed s/.bed/_intersect_/)
-    intersectBed -a $bedtools_dir/hg38_exons.bed -b $bed > $gtf_dir/$bedtools_output"exons.bed"
-    intersectBed -a $bedtools_dir/hg38_introns.bed -b $bed > $gtf_dir/$bedtools_output"introns.bed"
-    intersectBed -a $bedtools_dir/hg38_intergenic.bed -b $bed > $gtf_dir/$bedtools_output"intergenic.bed"
+    intersectBed -a $bedtools_dir/hg38_exons.bed -b $bed |sort -k1,1 -k2,2n | sortBed | mergeBed -i - > $gtf_dir/$bedtools_output"exons.bed"
+    intersectBed -a $bedtools_dir/hg38_introns.bed -b $bed |sort -k1,1 -k2,2n | sortBed | mergeBed -i - > $gtf_dir/$bedtools_output"introns.bed"
+    intersectBed -a $bedtools_dir/hg38_intergenic.bed -b $bed |sort -k1,1 -k2,2n | sortBed | mergeBed -i - > $gtf_dir/$bedtools_output"intergenic.bed"
 done
+
+echo "calculating jaccard ... " 
+#calculating intersection between the outputed genomic intersected bed files and reference genomic bed files using bedtools jaccard 
+for hgBed in $bedtools_dir/hg38_*.bed; do
+    regionName=$(echo "$(basename $hgBed)")
+    temp=$(echo $regionName | sed s/hg38_//)
+    for mergedBed in $paper_dir/*$temp; do
+        mBed=$(echo "$(basename $mergedBed)")
+	echo $regionName" jaccard "$mBed
+	bedtools jaccard -nonamecheck -a $hgBed -b $mergedBed
+    done >> $gtf_dir/jaccard.ods
+done 
