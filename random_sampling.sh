86808b004821cd7a8006d7f5844f960b68bb9628
Afnan <32648719+Afnan-Sultan@users.noreply.github.com>
2018-03-06 00:43:41 +0200

    Update random_sampling.sh

--- random_sampling.sh
+++ random_sampling.sh
@@ -4,10 +4,10 @@ paper_dir="$1"
 
 ##loop over libraries 
 for txt in $paper_dir/metadata* ; do
-    cat txt |
+    cat $txt |
     while read line; do
           lib_ribo_dir = $(echo $line | awk 'BEGIN{FS=",";} {print $1}')
-          lib_poly_name = $(echo $line | awk 'BEGIN{FS=",";} {print $9}')
+          lib_poly_dir = $(echo $line | awk 'BEGIN{FS=",";} {print $9}')
           lib_ribo_name = $(echo $line | awk 'BEGIN{FS=",";} {print $3}')
           lib_poly_name = $(echo $line | awk 'BEGIN{FS=",";} {print $11}')
           lib_ribo_len = $(echo $line | awk 'BEGIN{FS=",";} {print $4}')
@@ -22,7 +22,7 @@ for txt in $paper_dir/metadata* ; do
           seqtk sample -s$lib_len $lib_ribo_dir/fastq/$lib_ribo_name"_1.fastq.gz" > $lib_ribo_dir/RS_reads/$lib_ribo_name"_RS_1.fastq.gz"
           seqtk sample -s$lib_len $lib_ribo_dir/fastq/$lib_ribo_name"_2.fastq.gz" > $lib_ribo_dir/RS_reads/$lib_ribo_name"_RS_2.fastq.gz"
           seqtk sample -s$lib_len $lib_poly_dir/fastq/$lib_poly_name"_1.fastq.gz" > $lib_poly_dir/RS_reads/$lib_poly_name"_RS_1.fastq.gz"
-          seqtk sample -s$lib_len $lib_poly_dir/fastq/$lib_poly_name"_2.fastq.gz" > $lib_poly_ribo_dir/RS_reads/$lib_poly_name"_RS_2.fastq.gz"
+          seqtk sample -s$lib_len $lib_poly_dir/fastq/$lib_poly_name"_2.fastq.gz" > $lib_poly_dir/RS_reads/$lib_poly_name"_RS_2.fastq.gz"
     done
 done 
 
