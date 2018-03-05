ec520b0dd16cb2d8625b7939fe28162e62f3be12
Afnan <32648719+Afnan-Sultan@users.noreply.github.com>
2018-03-06 00:57:12 +0200

    Update random_sampling.sh

--- random_sampling.sh
+++ random_sampling.sh
@@ -6,16 +6,16 @@ paper_dir="$1"
 for txt in $paper_dir/metadata* ; do
     cat $txt |
     while read line; do
-          lib_ribo_dir = $(echo $line | awk 'BEGIN{FS=",";} {print $1}')
-          lib_poly_dir = $(echo $line | awk 'BEGIN{FS=",";} {print $9}')
-          lib_ribo_name = $(echo $line | awk 'BEGIN{FS=",";} {print $3}')
-          lib_poly_name = $(echo $line | awk 'BEGIN{FS=",";} {print $11}')
-          lib_ribo_len = $(echo $line | awk 'BEGIN{FS=",";} {print $4}')
-          lib_poly_len = $(echo $line | awk 'BEGIN{FS=",";} {print $12}')
+          lib_ribo_dir=$(echo $line | awk 'BEGIN{FS=",";} {print $1}')
+          lib_poly_dir=$(echo $line | awk 'BEGIN{FS=",";} {print $9}')
+          lib_ribo_name=$(echo $line | awk 'BEGIN{FS=",";} {print $3}')
+          lib_poly_name=$(echo $line | awk 'BEGIN{FS=",";} {print $11}')
+          lib_ribo_len=$(echo $line | awk 'BEGIN{FS=",";} {print $4}')
+          lib_poly_len=$(echo $line | awk 'BEGIN{FS=",";} {print $12}')
           if [ $lib_ribo_len > $lib_poly_len ]; then
-             lib_len = $lib_poly_len
+             lib_len=$lib_poly_len
           else
-             lib_len = $lib_ribo_len
+             lib_len=$lib_ribo_len
           fi
           mkdir $lib_ribo_dir/RS_reads
           mkdir $lib_poly_dir/RS_reads
