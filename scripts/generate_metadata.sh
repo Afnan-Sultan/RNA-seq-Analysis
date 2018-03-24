2eaec38cd8d415053f593acc59006a4949053a4a
Afnan <32648719+Afnan-Sultan@users.noreply.github.com>
2018-03-24 16:04:13 +0200

    Update generate_metadata.sh

--- scripts/generate_metadata.sh
+++ scripts/generate_metadata.sh
@@ -19,73 +19,95 @@
 paper_dir="$1"
 
 for lib_dir in $paper_dir/* ; do
-    lib_name=$(echo "$(basename $lib_dir)")
+	lib_name=$(echo "$(basename $lib_dir)")
     if [[ -d $lib_dir && ($lib_name == poly* || $lib_name == ribo*) ]]; then
-       for tissue_dir in $lib_dir/*; do
-           echo $tissue_dir
-           tissue_name=$(echo "$(basename $tissue_dir)")
-           echo $tissue_name
+		reads_len=$(
+		for tissue_dir in $lib_dir/*; do
+			#echo $tissue_dir
+		    tissue_name=$(echo "$(basename $tissue_dir)")
+		    #echo $tissue_name
+			for read in $tissue_dir/fastq/*_1.fastq.gz; do
+			   temp=$(echo "$(basename $read)")
+			   read_name=${temp%_1.fastq.gz}
+			   read_lines_len=$(zcat $read | wc -l)
+			   read_len=$(($read_lines_len/4))
+			   echo 
+			   echo $lib_name/$tissue_name','$read_name','$read_len #>> $paper_dir/$tissue_name"_unsorted.txt"
+			done
+		 done)
+     fi
+	 #echo $reads_len
+done 
 
-	   #sorting the libraries ascending by length 
-	   for read in $tissue_dir/fastq/*_1.fastq.gz; do
-	       temp=$(echo "$(basename $read)")
-	       read_name=${temp%_1.fastq.gz}
-
-	       #creating associative array to store the library ID as key, and the length as value
-	       declare -A libs_len
-               cat $paper_dir/acc_lists/SraRunTable.txt |
-	       while read line; do 
-	             lib_id=$(echo $line | awk 'BEGIN{FS=",";} {print $12}')
-		     read_id=$(echo $line | awk 'BEGIN{FS=",";} {print $11}')
-	             read_lines_len=$(zcat $read | wc -l)
-		     read_len=$(($read_lines_len/4))     		#as each spot is reported in 4 lines
-		     #echo $read_id $read_name
-		     #incrementing the liberary value by the read length associated with it
-		     if [ $read_id == $read_name ]; then
-			echo "inside if" 
-			if [ ${libs_len[$lib_id]+isset} ]; then
-			   echo "inside second if"
-			   libs_len[$lib_id]=$((${libs_len[$lib_id]}+$read_len))
-			else
-			   echo "inside else"
-			   libs_len[$lib_id]=$read_len
-			fi
-		     fi     
-	       done
-	       echo "${dict[@]}"
-	    done
-
-	    #sort the library IDs by their values and store them in a list
-	    sorted_lib_IDs=$(
+for lib_dir in $paper_dir/* ; do
+	lib_name=$(echo "$(basename $lib_dir)")
+    if [[ -d $lib_dir && ($lib_name == poly* || $lib_name == ribo*) ]]; then	
+		for tissue_dir in $lib_dir/*; do
+			tissue_name=$(echo "$(basename $tissue_dir)")
+			declare -A libs_len
+			echo 'array declared'
+			#echo "${!libs_len[@]}" "${libs_len[@]}"
+			for line1 in $reads_len; do
+				#echo "${!libs_len[@]}" "${libs_len[@]}"
+				read_name=$(echo $line1 | awk 'BEGIN{FS=",";} {print $2}')
+				read_len=$(echo $line1 | awk 'BEGIN{FS=",";} {print $3}')
+				#echo $read_name $read_len 
+				cat $paper_dir/acc_lists/SraRunTable.txt|
+				while read line2; do
+					#echo "${!libs_len[@]}" "${libs_len[@]}"
+					lib_id=$(echo $line2 | awk 'BEGIN{FS=",";} {print $12}')
+				 	read_id=$(echo $line2 | awk 'BEGIN{FS=",";} {print $11}')
+					#echo $lib_id 
+					#echo "${libs_len[@]}"
+				 	if [[ $read_id == $read_name ]]; then
+						echo "inside if"
+						echo $line1",$lib_id" >> $paper_dir/$tissue_name"_unsorted.txt"
+						if [[ ${libs_len[$lib_id]+isset} ]]; then
+				   			echo "inside second if"
+				   			libs_len[$lib_id]=$((${libs_len[$lib_id]}+$read_len))
+							#echo "${libs_len[@]}"
+						else
+				   			echo "inside else"
+				   			libs_len[$lib_id]=$read_len
+							echo "${!libs_len[@]}" "${libs_len[@]}"
+						fi
+						break
+				 	fi
+				 done
+			done
+			echo "${libs_len[@]}"
+			sorted_lib_IDs=$(
             for lib in "${!libs_len[@]}"; do
-		echo $lib ',' ${libs_len["$lib"]}
-		done | sort -n -k3 | sed 's/,.*//') 
-	    echo "sorted lbs" $sorted_lib_IDs
-	    #writting the requiered fields in the metada file for each tissue 
-	    for lib in $sorted_lib_IDs; do
-    		cat $paper_dir/acc_lists/SraRunTable.txt |
-	        while read line; do
-                      lib_id=$(echo $line | awk 'BEGIN{FS=",";} {print $12}') 
-                      if [[ $lib == $lib_id ]]; then
-			 read_id=$(echo $line | awk 'BEGIN{FS=",";} {print $11}')
-	             	 read_lines_len=$(zcat $read | wc -l)
-		     	 read_len=$((rea_len/4))
-			 echo $tissue_name','$lib_id','$read_id','$read_len >> $paper_dir/$tissue_name".txt"
-		      fi
-		done
-	    done	
-       done
-    fi
-done
-
+				echo $lib ',' ${libs_len["$lib"]}
+			done | sort -n -k3 | sed 's/,.*//') 
+	    	echo "sorted libs" $sorted_lib_IDs
+	    	for lib in $sorted_lib_IDs; do
+				cat $paper_dir/$tissue_name"_unsorted.txt"|
+				while read line; do
+					lib_id=$(echo $line | awk 'BEGIN{FS=",";} {print $4}')
+					if [ $lib == $lib_id ]; then
+						echo $line >> $paper_dir/$tissue_name"sorted.txt"
+					fi
+				done
+				end=$(cat $paper_dir/$tissue_name"sorted.txt" | wc -l)
+				for i in $(seq 0 8 $end); do
+					echo 'x' | ex -s -c 'i,$((i+8))!sort -k3 -n' $paper_dir/$tissue_name"sorted.txt"
+				done	
+			done
+		done 
+	fi
+done 
 #merging poly and ribo liberaries from the same tissue in one file for easier comparison later.
 for txt in $paper_name/poly*; do
-    file1=$(echo "$(basename $txt)")
-    out=$(echo $file1 | sed 's/poly_//')
-    if [[ txt == poly_brain.txt ]]; then
-       file2=ribo_brain.tx
-    else
-       file2=ribo_tumor.txt
-    paste -d "," $paper_dir/file1 $paper_dir/file2 > $paper_dir/metadata"_$out"
-    fi
-done 
+	if [ $txt == *_sorted.txt ]; then
+		file1=$(echo "$(basename $txt)")
+		out=$(echo $file1 | sed 's/poly_//')
+		if [[ txt == poly_brain.txt ]]; then
+		   file2=ribo_brain.tx
+		else
+		   file2=ribo_tumor.txt
+		fi
+		paste -d "," $paper_dir/file1 $paper_dir/file2 > $paper_dir/metadata"_$out"
+	fi
+done  
+
