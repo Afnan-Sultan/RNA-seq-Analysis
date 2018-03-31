5de94862c0ba9c4c683f423c502355f99e22dd96
Afnan <32648719+Afnan-Sultan@users.noreply.github.com>
2018-03-31 13:32:41 +0200

    Update random_sampling.sh

--- scripts/random_sampling.sh
+++ scripts/random_sampling.sh
@@ -2,26 +2,31 @@
 
 paper_dir="$1"
 
-for txt in $paper_dir/metadata* ; do
-    cat $txt |
-    while read line; do
-          lib_ribo_dir = $(echo $line | awk 'BEGIN{FS=",";} {print $1}')
-          lib_poly_dir = $(echo $line | awk 'BEGIN{FS=",";} {print $8}')
-          lib_ribo_name = $(echo $line | awk 'BEGIN{FS=",";} {print $3}')
-          lib_poly_name = $(echo $line | awk 'BEGIN{FS=",";} {print $10}')
-          lib_ribo_len = $(echo $line | awk 'BEGIN{FS=",";} {print $4}')
-          lib_poly_len = $(echo $line | awk 'BEGIN{FS=",";} {print $11}')
-          if [ $lib_ribo_len > $lib_poly_len ]; then
-             lib_len=$lib_poly_len
-          else
-             lib_len=$lib_ribo_len
-          fi
-          mkdir $lib_ribo_dir/RS_reads
-          mkdir $lib_poly_dir/RS_reads
-          seqtk sample -s$lib_len $lib_ribo_dir/fastq/$lib_ribo_name"_1.fastq.gz" > $lib_ribo_dir/RS_reads/$lib_ribo_name"_RS_1.fastq.gz"
-          seqtk sample -s$lib_len $lib_ribo_dir/fastq/$lib_ribo_name"_2.fastq.gz" > $lib_ribo_dir/RS_reads/$lib_ribo_name"_RS_2.fastq.gz"
-          seqtk sample -s$lib_len $lib_poly_dir/fastq/$lib_poly_name"_1.fastq.gz" > $lib_poly_dir/RS_reads/$lib_poly_name"_RS_1.fastq.gz"
-          seqtk sample -s$lib_len $lib_poly_dir/fastq/$lib_poly_name"_2.fastq.gz" > $lib_poly_dir/RS_reads/$lib_poly_name"_RS_2.fastq.gz"
-    done
-done 
-
+cat $paper_dir/metadata.txt |
+while read line; do
+	echo new line
+	lib_ribo=$(echo $line | awk 'BEGIN{FS=",";} {print $1}')
+    tissue_ribo=$(echo $line | awk 'BEGIN{FS=",";} {print $2}')
+	read_ribo=$(echo $line | awk 'BEGIN{FS=",";} {print $4}')
+	read_ribo_len=$(echo $line | awk 'BEGIN{FS=",";} {print $5}')
+	lib_poly=$(echo $line | awk 'BEGIN{FS=",";} {print $6}')
+    tissue_poly=$(echo $line | awk 'BEGIN{FS=",";} {print $7}')
+    read_poly=$(echo $line | awk 'BEGIN{FS=",";} {print $9}')
+    read_poly_len=$(echo $line | awk 'BEGIN{FS=",";} {print $10}')
+    if [ $read_ribo_len -gt $read_poly_len ]; then
+		lib_len=$read_poly_len
+    else
+        lib_len=$read_ribo_len
+    fi
+   	mkdir $paper_dir/$lib_ribo/$tissue_ribo/RS_reads
+    mkdir $paper_dir/$lib_poly/$tissue_poly/RS_reads
+	echo $lib_len
+	zcat $paper_dir/$lib_ribo/$tissue_ribo/fastq/$read_ribo"_1.fastq.gz"|
+    seqtk sample - $lib_len > $paper_dir/$lib_ribo/$tissue_ribo/RS_reads/$read_ribo"_RS_1.fastq"
+	zcat $paper_dir/$lib_ribo/$tissue_ribo/fastq/$read_ribo"_2.fastq.gz"|
+    seqtk sample - $lib_len > $paper_dir/$lib_ribo/$tissue_ribo/RS_reads/$read_ribo"_RS_2.fastq"
+	zcat $paper_dir/$lib_poly/$tissue_poly/fastq/$read_poly"_1.fastq.gz"|
+    seqtk sample - $lib_len > $paper_dir/$lib_poly/$tissue_poly/RS_reads/$read_poly"_RS_1.fastq"
+	zcat $paper_dir/$lib_poly/$tissue_poly/fastq/$read_poly"_2.fastq.gz"|
+    seqtk sample - $lib_len > $paper_dir/$lib_poly/$tissue_poly/RS_reads/$read_poly"_RS_2.fastq"
+done
