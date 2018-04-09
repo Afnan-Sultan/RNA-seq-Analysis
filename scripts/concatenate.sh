013750d2fef623753fdbc8d08e7e5027ca129eb1
drtamermansour <drtamermansour@gmail.com>
2018-04-09 00:50:43 -0400

    fix2 concatenate.sh

--- scripts/concatenate.sh
+++ scripts/concatenate.sh
@@ -14,15 +14,15 @@ for lib_dir in $paper_dir/* ; do
 	      mkdir -p $tissue_dir/merged_reads
 	      for read in $tissue_dir/RS_reads/*_1.fastq; do
 	          temp=$(echo "$(basename $read)")
-		  read_name=${temp%_1.fastq}
+		  read_name=${temp%_RS_1.fastq}
                   cat $paper_dir/acc_lists/SraRunTable.txt |
                   while read line; do 	
 		        sample_id=$(echo $line | awk 'BEGIN{FS=",";} {print $3}')
 		        read_id=$(echo $line | awk 'BEGIN{FS=",";} {print $11}')
 		        if [[ $read_id == $read_name ]]; then
                            echo "Add $read_id to $sample_id"
-                           cat $tissue_dir/RS_reads/$read_id"_1.fastq" >> $tissue_dir/merged_reads/$sample_id"_1.fastq" 
-		           cat $tissue_dir/RS_reads/$read_id"_2.fastq" >> $tissue_dir/merged_reads/$sample_id"_2.fastq" 
+                           cat $tissue_dir/RS_reads/$read_id"_RS_1.fastq" >> $tissue_dir/merged_reads/$sample_id"_1.fastq" 
+		           cat $tissue_dir/RS_reads/$read_id"_RS_2.fastq" >> $tissue_dir/merged_reads/$sample_id"_2.fastq" 
 		        fi
 	          done
 	       done
