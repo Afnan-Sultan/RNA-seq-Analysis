8ae5379aec4a2c0d3089140b63f5e1d248f2a384
drtamermansour <drtamermansour@gmail.com>
2018-06-02 05:40:14 -0400

    bamToBed

--- scripts/BamToBed.sh
+++ scripts/BamToBed.sh
@@ -2,7 +2,7 @@
 
 paper_dir="$1"
 pipeline_dir="$2"
-platform="$3"
+plateform="$3"
 bed_files_dir="$4"
 assembler="$5"
 paper_name=$(echo "$(basename $paper_dir)")
@@ -10,6 +10,8 @@ paper_name=$(echo "$(basename $paper_dir)")
 while read tissue;do
 	tissue_dir=$pipeline_dir/$paper_name/$tissue
 	for bam in $tissue_dir/*.bam; do
+                if [ $plateform == "HPC" ];then
+                  module load BEDTools/2.24.0;fi
 		bamToBed -i $bam |sort -k1,1 -k2,2n | sortBed >> $bed_files_dir/$tissue"_"$assembler"_bamToBed.bed"
 	done
 done < $paper_dir/tissues.txt
