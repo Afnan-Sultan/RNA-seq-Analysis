8a6a9dfab236936f36d0e5bc436cdce53d7720a1
Afnan <32648719+Afnan-Sultan@users.noreply.github.com>
2019-02-13 19:47:51 +0100

    Update cuffmerge.sh

--- scripts/cuffmerge.sh
+++ scripts/cuffmerge.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 paper_dir="$1"
-star_dir="$2"
-merged_gtf_dir="$3"
-plateform="$4"
+aligner_dir="$2"
+merged_gtf_dir="$4"
+plateform="$3"
 paper_name=$(echo "$(basename $paper_dir)")
 
 if [ "$plateform" == "HPC" ];then 
   module load Python/2.7.2; module swap GNU GNU/4.4.5; module load cufflinks/2.2.1;echo "module loading done";fi
 while read tissue;do
-    tissue_dir=$star_dir/$paper_name/$tissue
-    cuffMerge_output=$(echo "$(basename $tissue_dir"_scallop_merged.gtf")")
-    cuffmerge -o $star_dir/$paper_name/ <(ls $tissue_dir/*.gtf)
-    mv $star_dir/$paper_name/merged.gtf $merged_gtf_dir/$paper_name/$cuffMerge_output
+    tissue_dir=$aligner_dir/$paper_name/$tissue
+    assembler=$(echo "$(basename $aligner_dir)"| sed s/.*-//)
+    cuffMerge_output=$(echo "$(basename $tissue_dir"_"$assembler"_merged.gtf")")
+    cuffmerge -o $aligner_dir/$paper_name/ <(ls $tissue_dir/*.gtf)
+    mv $aligner_dir/$paper_name/merged.gtf $merged_gtf_dir/$paper_name/$cuffMerge_output
 done < $paper_dir/tissues.txt
 
 
