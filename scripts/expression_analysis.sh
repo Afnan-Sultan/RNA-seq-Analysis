1298be8aba04fb853042b12cbb701274c5a98f49
Afnan-Sultan <s-afnan.sultan@zewailcity.edu.eg>
2019-08-28 22:39:21 +0200

    update expression_analysis.sh

--- scripts/expression_analysis.sh
+++ scripts/expression_analysis.sh
@@ -8,14 +8,14 @@ paper_dir="$4"
 echo "################prepare Transcriptome.fasta########################"
 
 #the origial gtf_genome_to_cdna_fasta.pl file wasn't compatiple with the gencode gtf version as it contained more info than required, and the code terminated due to insignificant error to the needed analysis. So, the modified version comments an if condition to allow the analysis to move on.  
-cp $work_dir/scripts/modified_gtf_genome_to_cdna_fasta.pl $prog_dir/TransDecoder-TransDecoder-v5.5.0/util
+cp $work_dir/scripts/modified_gtf_genome_to_cdna_fasta.pl $prog_dir/TransDecoder-TransDecoder-v5.5.0/util/
 
-.$prog_dir/TransDecoder-TransDecoder-v5.5.0/util/modified_gtf_genome_to_cdna_fasta.pl $index_dir_path/gencode.v27.primary_assembly.annotation.gtf $index_dir_path/GRCh38.primary_assembly.genome.fa > $index_dir_path/transcripts.fasta
+#perl $prog_dir/TransDecoder-TransDecoder-v5.5.0/util/modified_gtf_genome_to_cdna_fasta.pl $index_dir_path/gencode.v27.primary_assembly.annotation.gtf $index_dir_path/GRCh38.primary_assembly.genome.fa > $index_dir_path/transcripts.fasta
 
 echo "####################quantifying reads########################"
 
 #build transcriptome.fasta index
-.$prog_dir/salmon-latest_linux_x86_64/bin/salmon index -t $index_dir_path/transcripts.fasta -i $index_dir_path/salmon_transcripts_index
+$prog_dir/salmon-latest_linux_x86_64/bin/salmon index -t $index_dir_path/transcripts.fasta -i $index_dir_path/salmon_transcripts_index
 
 #quantfication
 ##loop over libraries 
@@ -31,7 +31,7 @@ for lib_dir in $paper_dir/* ; do
 	      for read in $tissue_dir/trimmed_reads/*_1.fastq.gz; do
 		read_2=$(echo $read | sed s/_1.fastq.gz/_2.fastq.gz/)
 		read_name=$(echo "$(basename $read)" | sed s/_1.fastq.gz//)
-		.$prog_dir/salmon-latest_linux_x86_64/bin/salmon quant -i $index_dir_path/salmon_transcripts_index -l A -1 $read -2 $read_2 -p 8 --validateMappings -o expression_analysis/$lib_name/$tissue_name/$read_name"_quant"
+		$prog_dir/salmon-latest_linux_x86_64/bin/salmon quant -i $index_dir_path/salmon_transcripts_index -l A -1 $read -2 $read_2 -p 8 --validateMappings -o expression_analysis/$lib_name/$tissue_name/$read_name"_quant"
 	      done
 	   fi
 	done
